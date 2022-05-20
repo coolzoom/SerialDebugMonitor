b9054dad88b7168b555a673a6e4ca2411fea48be
zz <zz@test.com>
2022-05-20 14:53:40 +0800

    code clean

--- serialDebugMonitor copy.py
+++ serialDebugMonitor copy.py
@@ -1,13 +1,9 @@
 
 import logging
 import datetime
-import queue
-
 import serial
 import serial.tools.list_ports as port_list
-
 import threading
-
 import time
 
 class SerialMonitor():
@@ -38,18 +34,7 @@ class SerialMonitor():
         self._writeThread = None
         
         self._conn = None
-        self._recievedQueue = queue.Queue()
-        self.maxSerialChars = 10*1000
 
-
-
-    ##
-    ## @brief      Stop all active tasks
-    ##
-    ## @param      self  The object
-    ##
-    ## @return     None
-    ##
     def stopAllTasks(self):
         try:
             # stop all timer here
@@ -63,14 +48,6 @@ class SerialMonitor():
         except Exception as e:
             self.logger.warning(e)
     
-    ##
-    ## @brief      Read the USB port in an endless loop
-    ##
-    ## @param      self     The object
-    ## @param      running  Bool to stay in the while loop
-    ##
-    ## @return     None
-    ##
     def read(self, running, connection):
         self._runReadThread = running
 
@@ -116,27 +93,12 @@ class SerialMonitor():
                     messageDict["timestamp"] = unixMicros
                     messageDict["message"] = str(line,'utf-8') #bydefault it output byte, like b'echo:Home offset:\n', so we need to assign encoding
                     print(str(line,'utf-8'))
-                    # add this message dict to the received queue
-                    # self._recievedQueue.put(messageDict)
-
                     # AppendText is not thread safe!
                     # self.txtSerialMonitor.AppendText(line)
                     self.fillSerialConsole(data=messageDict)
 
                 time.sleep(0.1)
 
-    """
-    def getReceiveQueue(self):
-        return self._recievedQueue
-    """
-
-    ##
-    ## @brief      Starts the receiving thread.
-    ##
-    ## @param      self  The object
-    ##
-    ## @return     None
-    ##
     def startReceivingThread(self):
         self.pauseReceivingThread(pause=False)
         self._receivingThread = threading.Thread(
@@ -146,25 +108,10 @@ class SerialMonitor():
             name="ReadingThread")
         self._receivingThread.start()
 
-    ##
-    ## @brief      Pause receiving thread
-    ##
-    ## @param      self   The object
-    ## @param      pause  The pause
-    ##
-    ## @return     None
-    ##
     def pauseReceivingThread(self, pause=False):
         self.logger.info("Pausing receiving thread: %s" %(pause))
         self._runReadThread = not pause
 
-    ##
-    ## @brief      Stop the receiving thread
-    ##
-    ## @param      self  The object
-    ##
-    ## @return     None
-    ##
     def stopReceivingThread(self):
         self.logger.info("Stopping receiving thread now")
         self._runReadThread = False
@@ -175,76 +122,21 @@ class SerialMonitor():
 
             del self._receivingThread
 
-    ##
-    ## @brief      Gets the receiving thread state.
-    ##
-    ## @param      self  The object
-    ##
-    ## @retval     True     Running receiving commands from device
-    ## @retval     False    Not receiving commands from device
-    ##
     def getReceivingThreadState(self):
         return self._runReadThread
 
-    ##
-    ## @brief      Gets the unix timestamp in micros.
-    ##
-    ## @param      self  The object
-    ##
-    ## @return     The unix timestamp in micros.
-    ##
     def getUnixMicrosTimestamp(self):
         # given in seconds, multiply by 1000 to get millis, again times 1000 to get micros
         return int(time.time()*1000*1000)
 
-    ##
-    ## @brief      Gets the current time.
-    ##
-    ## Format is Hour:Minutes:Seconds:Microseconds
-    ##
-    ## @param      self  The object
-    ##
-    ## @return     The timestamp as string.
-    ##
     def getCurrentTime(self):
         return datetime.datetime.now().strftime("%H:%M:%S:%f")
 
-    ##
-    ## @brief      Return runtime of the app
-    ##
-    ## @param      self  The object
-    ##
-    ## @return     The runtime
-    ##
     def getRuntime(self):
         stopTime = datetime.datetime.now()  # get current time
         theRunTime = stopTime - self.startTime  # time difference aka runtime
 
         return theRunTime
-
-    def restorePortSelection(self, portString):
-        matchingIndexList = list()
-
-        # search for matching string in available ports, if portString not ""
-        if len(portString):
-            # search for matches of self.defaultPort in available ports list
-            matchingIndexList = [idx for idx, val in enumerate(self.availablePorts) if portString in val]
-
-        # comboBox selection is done by index, -1 is None/empty selection
-        matchingIndex = -1
-
-        if len(matchingIndexList):
-            # take first match if list contains at least 1 element
-            matchingIndex = matchingIndexList[0]
-        else:
-            pass
-            # specify explicit value or use wx constant
-            # matchingIndex = wx.NOT_FOUND
-
-            # self.logger.warning("Specified defaultPort is not available for selection, using None/empty")
-
-        # pre-select the port of the port combo box
-        self.cmbPorts.SetSelection(matchingIndex)
 
     def fillSerialConsole(self, data):
         # build message string
@@ -288,20 +180,14 @@ class SerialMonitor():
             if self._receivingThread != None:
                 # stop any (may already running) receiving thread
                 self.stopReceivingThread()
-
             self._conn.close()
-
             #self.stopAllTasks()
-
             self.logger.debug("Port is closed, ready to open")
         else:
             # connection not yet open
             self.logger.debug("Port is not open, opening now")
-
             self._conn.open()
-
             time.sleep(0.1)
-
             # start the receiving thread
             self.startReceivingThread()
 
@@ -330,14 +216,7 @@ class SerialMonitor():
             # wait up to 1 second until thread terminates
             self._writeThread.join(1)
             del self._writeThread
-    ##
-    ## @brief      Quit app and stop all tasks
-    ##
-    ## @param      self   The object
-    ## @param      event  The event
-    ##
-    ## @return     None
-    ##
+
     def CloseApp(self):
         logger = logging.getLogger(__name__)
 
@@ -360,7 +239,7 @@ ser.WriteCommand('G28')
 
 # test write thread, this is probably not useful for production
 ser.startWriteThread()
-time.sleep(100)
+time.sleep(30)
 
 # stop write thread
 ser.stopWriteThread()
